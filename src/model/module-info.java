7ff43e3063ab8af958188ed972cfc5a097705a6c
Jonathan Nakagawa <jyn2@duke.edu>
2018-10-12 17:45:16 -0400

    Added module info files

---
+++ src/model/module-info.java
@@ -0,0 +1,8 @@
+module slogo_team03.model {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+
+    exports commands;
+}
---
+++ src/view/module-info.java
@@ -0,0 +1,7 @@
+module slogo_team03.view {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+    
+}
