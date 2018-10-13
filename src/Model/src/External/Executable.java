8c07960e08d50f73bcd2164e88ce42196e94a323
Jason Zhou <jz192@duke.edu>
2018-10-13 15:54:39 -0400

    added comments to interface

--- src/Model/src/External/Executable.java
+++ src/Model/src/External/Executable.java
@@ -1,9 +1,20 @@
 package External;
 
+/**
+ * An interface used for classes that encapsulate the procedures with their parameters called by the commands
+ */
+
 public interface Executable {
 
-    public void execute();
+    /**
+     * Takes the turtle and manipulates according to the specific command object
+     * @param turtle The model of the turtle
+     */
+    public void execute(ModelTurtle turtle);
 
+    /**
+     * Returns the value that from the execution of the command object
+     */
     public int returnVal();
 
 }
--- src/Model/src/Internal/CommandFactory.java
+++ src/Model/src/Internal/CommandFactory.java
@@ -2,7 +2,11 @@ package Internal;
 import External.Executable;
 
 public interface CommandFactory {
-
+    /**
+     * Takes a command string and creates a Executable object
+     * @param cmd A string that stands for the command
+     * @param params A list of parameters for the command
+     */
     public Executable createCommand(String cmd, List params);
 
 
