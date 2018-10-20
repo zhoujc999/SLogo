e5115478dab1fd09b8e025293ab3c0896bbae66f
Jonathan Nakagawa <jyn2@duke.edu>
2018-10-20 19:36:48 -0400

    Added node classes to hadle command parsing

---
+++ src/Model/src/external/Node.java
@@ -0,0 +1,32 @@
+package external;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Node {
+
+    /**
+     * Gets the data stored in node
+     * @return the data stored in node
+     */
+    public Object getData();
+
+    /**
+     * Gets the array list of child nodes of the current node
+     * @return the array list of child nodes of the current node
+     */
+    public List<? extends Node> getChildren();
+
+    /**
+     * Adds a new child to the node
+     * @param nwChild the new node that is being added as a child of the current node
+     */
+    public void addChild(Node nwChild) throws IllegalAccessException;
+
+    /**
+     * Sets the data of the current node
+     * @param nwData the data of the current node
+     */
+    public void setData(String nwData);
+
+}
