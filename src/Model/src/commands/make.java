6414b5a7a4c30b79a9790eac9d9d0ff1553378fb
Jason Zhou <jz192@duke.edu>
2018-10-21 19:32:32 -0400

    updated model

---
+++ src/Model/src/commands/make.java
@@ -0,0 +1,38 @@
+package commands;
+
+import external.ModelTurtle;
+import external.Parse;
+import external.SLogoAbstractExecutable;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class make implements SLogoAbstractExecutable {
+    private final static int numParams = 2;
+    private String param1;
+    private double param2;
+    private double result;
+    private Consumer<Parse> c;
+
+
+
+    public make(List params) {
+        if (params.size() != numParams) {
+            throw new IllegalArgumentException("Argument Length Error");
+        }
+        try {
+            param1 = (String) params.get(0);
+            param2 = Double.parseDouble((String) params.get(1));
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void execute(ModelTurtle turtle) {
+        c = (p) -> {
+            p.
+
+        }
+    }
+}
