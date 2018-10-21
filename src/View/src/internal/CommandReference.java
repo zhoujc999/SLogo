b15128c12f4fe16a13c28a48eb1d3da7d71ec976
Tahj Starr <tms62@duke.edu>
2018-10-21 14:32:29 -0400

    Added Command Reference

--- src/View/src/internal/CommandReference.java
+++ src/View/src/internal/CommandReference.java
@@ -1,6 +1,16 @@
 package internal;
 
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
 
 public class CommandReference extends Stage {
     /**
@@ -11,6 +21,42 @@ public class CommandReference extends Stage {
      * @author Tahj Starr
      */
 
+    private static final Font COMMAND_CATEGORIES_FONT = new Font("Agency FB", 30);
+    private static final Font DESCRIPTIONS_FONT = new Font(10);
+
+    private String DEFAULT_RESOURCES = "Reference";
+    private ResourceBundle myCommandCategories;
+    private String myLanguage;
+
+    public CommandReference(String language) {
+        myLanguage = language;
+        myCommandCategories = ResourceBundle.getBundle(DEFAULT_RESOURCES + language + "/CommandCategories");
+        setScene(new Scene(writePage(), 600, 800));
+    }
+
+    private Parent writePage() {
+        var page = new VBox();
+        var commandCategories = myCommandCategories.getKeys();
+        while (commandCategories.hasMoreElements()) {
+            String category = commandCategories.nextElement();
+
+            var categoryText = new Text(category);
+            categoryText.setFont(COMMAND_CATEGORIES_FONT);
+            page.getChildren().add(categoryText);
+
+            var descriptionResources = ResourceBundle.getBundle(DEFAULT_RESOURCES + myLanguage + "/" + category);
+            var descriptions = descriptionResources.getKeys();
+            while (descriptions.hasMoreElements()) {
+                String description = descriptions.nextElement();
+
+                var descriptionText = new Text(description + " : " + descriptionResources.getString(description));
+                descriptionText.setFont(DESCRIPTIONS_FONT);
+                page.getChildren().add(descriptionText);
+            }
+        }
+        return new ScrollPane(page);
+    }
+
 //    /**
 //     * Shows command reference page in a another stage.
 //     */
@@ -20,10 +66,12 @@ public class CommandReference extends Stage {
 //     * Hides the command reference page.
 //     */
 //    void hide();
-//
-//    /**
-//     * Sets the language of the text in CommandReference.
-//     */
-//    void setLanguage(String language);
+
+    /**
+     * Sets the language of the text in CommandReference.
+     */
+    void setLanguage(String language) {
+        myLanguage = language;
+    }
 
 }
