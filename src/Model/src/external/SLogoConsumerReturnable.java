605f18004fc711c3dcc1a14f59bd77ae3e28a163
Jason Zhou <jz192@duke.edu>
2018-10-24 02:20:34 -0400

    added SLogoReturnable

--- src/Model/src/external/SLogoConsumerReturnable.java
+++ src/Model/src/external/SLogoConsumerReturnable.java
@@ -2,7 +2,7 @@ package external;
 
 import java.util.function.Consumer;
 
-public interface SLogoConsumerReturnable {
+public interface SLogoConsumerReturnable extends SLogoReturnable<Consumer> {
     /**
      * Returns the command that from the execution of the command object
      */
