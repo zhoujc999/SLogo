4c7f7c48fcae588d45eb0941b0f5e6ed4f7ae247
Jay Pande <jgp17@duke.edu>
2018-10-25 21:44:26 -0400

    added previously uncommitted changes to invoker

--- src/Model/src/Invoking/Invoker.java
+++ src/Model/src/Invoking/Invoker.java
@@ -3,13 +3,26 @@ package invoking;
 import external.*;
 
 import java.util.Observable;
+import java.util.function.Consumer;
 
 
 public class Invoker extends Observable implements Invokable{
-    private ModelTurtle myTurt;
+    private StdModelTurtle myTurt;
+    private Consumer<StdModelTurtle> addObserver;
 
     public Invoker(){
         myTurt = new StdModelTurtle(0);
+    }
+
+    /**
+     * Use this method to add the graphicsWindow as an observer for all turtles once the graphicsWindow
+     * has been instantiated (it is instantiated when the GUI is instantiated)
+     *
+     * @param addObserver (StdModelTurtle turt) -> turt.addObserver(graphicsWindow)
+     */
+    public void setAddObserver(Consumer<StdModelTurtle> addObserver) {
+        this.addObserver = addObserver;
+        this.addObserver.accept(myTurt);
     }
 
     @Override
