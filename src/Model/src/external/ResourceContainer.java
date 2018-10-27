0cefeee2175946802f3423a01dafa5a58d84e3b2
Jonathan Nakagawa <jyn2@duke.edu>
2018-10-27 13:19:54 -0400

    Added interface for resource handling

---
+++ src/Model/src/external/ResourceContainer.java
@@ -0,0 +1,32 @@
+package external;
+
+public interface ResourceContainer {
+
+    /**
+     * Gets the type of the word provided
+     * @param word The original word
+     * @return The type of the original word
+     */
+    public String getType(String word);
+
+    /**
+     * Translates the given word
+     * @param word The original untranslated word
+     * @return The translated word
+     */
+    public String getTranslation(String word);
+
+    /**
+     * Changes the language used for getting translations
+     * @param nwLang The name of the new language
+     */
+    public void changeLanguage(String nwLang);
+
+    /**
+     * Returns the number of parameters needed for the given command
+     * @param command The name of the command
+     * @return The number of parameters specified
+     */
+    public Integer getParameterCount(String command);
+
+}
