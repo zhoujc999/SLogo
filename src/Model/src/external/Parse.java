5358aa852b08b5885c97488f3fd9511a40965412
Jonathan Nakagawa <jyn2@duke.edu>
2018-10-27 14:00:29 -0400

    Configured parser and invorker to work with new executor and builder interfaces

--- src/Model/src/external/Parse.java
+++ src/Model/src/external/Parse.java
@@ -30,11 +30,16 @@ public interface Parse extends Observer {
     public void update(Observable o, Object arg);
 
     /**
-     * Changes the language that the Parser recognizes for commands
-     * @param lang The new language
+     * Changes receives the values of the change
+     * @param s
      */
 
     public void setReplacementValue(String s);
+
+    /**
+     * Changes the language that the Parser recognizes for commands
+     * @param lang The new language
+     */
 
     public void changeLanguage(String lang);
 
