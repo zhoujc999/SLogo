2140d7f7629cf0cc9fc766b32d4ab9a4dd332525
Tahj Starr <tms62@duke.edu>
2018-10-27 01:14:33 -0400

    Updated main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import external.ModelPen;
 import external.ModelTurtle;
+import external.StdModelTurtle;
 import invoking.Invoker;
 import commandFactory.CommandFactory;
 import commandFactory.CommandFactoryInterface;
@@ -22,9 +23,12 @@ public class Main extends Application {
     private static final String TITLE = "SLogo";
     private static final Dimension2D SIZE = new Dimension2D(800, 600);
     private static final String DEFAULT_LANGUAGE = "English";
-//    private static final Map<String, Consumer<ModelTurtle>> consumerMap = Map.ofEntries(
-//            Map.entry("onFd", (turt) -> turt.forward(5.0),
-//                    Map.entry("onTurn",  (turt) -> turt.right(5.0))));
+    private static final Map<String, Consumer<ModelTurtle>> consumerMap = Map.ofEntries(
+            Map.entry("fd", (turt) -> turt.forward(5.0)),
+            Map.entry("bk", (turt) -> turt.forward(5.0)),
+            Map.entry("rt",  (turt) -> turt.right(5.0)),
+            Map.entry("lt",  (turt) -> turt.left(5.0))
+    );
 
     @Override
     public void start(Stage primaryStage) {
@@ -32,8 +36,13 @@ public class Main extends Application {
         Invokable invoker = new Invoker();
         CommandFactoryInterface myFactory = new CommandFactory(invoker);
         Parser myParser = new Parser(myFactory, DEFAULT_LANGUAGE);
+//        BiConsumer<Integer, String> IDMethodConsumer = (id, method) -> invoker.getTurtles;
         GUI gui = new GUI(DEFAULT_LANGUAGE, myParser::parseCommand);
-        primaryStage.setScene(new Scene(gui, SIZE.getWidth(), SIZE.getHeight()));
+        Consumer<StdModelTurtle> turtleObserverConsumer = (turt) -> turt.addObserver(gui.getGraphicsWindow());
+        ( (Invoker) invoker ).setAddObserver(turtleObserverConsumer);
+        Scene scene = new Scene(gui, SIZE.getWidth(), SIZE.getHeight());
+//        scene.setOnKeyPressed();
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
