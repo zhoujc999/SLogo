fe14ca6b4e77d05ad9f850d14494ccda38baf4f1
Jonathan Nakagawa <jyn2@duke.edu>
2018-10-28 18:00:02 -0400

    Moved methods from parser interface to executor

--- src/Model/src/external/Parse.java
+++ src/Model/src/external/Parse.java
@@ -29,12 +29,12 @@ public interface Parse extends Observer {
 
     public void update(Observable o, Object arg);
 
-    /**
-     * Changes receives the values of the change
-     * @param s
-     */
-
-    public void setReplacementValue(String s);
+//    /**
+//     * Changes receives the values of the change
+//     * @param s
+//     */
+//
+//    public void setReplacementValue(String s);
 
     /**
      * Changes the language that the Parser recognizes for commands
@@ -43,11 +43,11 @@ public interface Parse extends Observer {
 
     public void changeLanguage(String lang);
 
-    /**
-     * Adds a variable to the map of variables that the parser recognizes
-     * @param name The name of the variable being added
-     * @param value The value mapped to the variable
-     */
-    public void addVariable(String name, String value);
+//    /**
+//     * Adds a variable to the map of variables that the parser recognizes
+//     * @param name The name of the variable being added
+//     * @param value The value mapped to the variable
+//     */
+//    public void addVariable(String name, String value);
 
 }
--- src/Model/src/external/TreeExecutor.java
+++ src/Model/src/external/TreeExecutor.java
@@ -4,6 +4,19 @@ public interface TreeExecutor {
 
     public void executeTree(Node nd, ResourceContainer container);
 
+
+    /**
+     * Changes receives the values of the change
+     * @param s
+     */
+
     public void setReplacementValue(String s);
+
+    /**
+     * Adds a variable to the map of variables that the parser recognizes
+     * @param name The name of the variable being added
+     * @param value The value mapped to the variable
+     */
+    public void addVariable(String name, String value);
 
 }
