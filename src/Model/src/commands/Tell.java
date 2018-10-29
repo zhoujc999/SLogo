7cd5d704ec7b3912eb00210bc76652e7ba7d8324
Jason Zhou <jz192@duke.edu>
2018-10-29 01:46:29 -0400

    added mult turtles

--- src/Model/src/commands/Tell.java
+++ src/Model/src/commands/Tell.java
@@ -1,4 +1,30 @@
 package commands;
 
-public class Tell {
+import external.*;
+import parsing.PentaConsumer;
+
+import java.util.List;
+
+public class Tell extends UnaryOperator implements SLogoMultiExecutable, SLogoConsumerReturnable {
+
+    String[] turtlesIDList;
+    private PentaConsumer<Parse, TreeExecutor, VariableManipulator, ParameterChangeInterface, Invokable> c;
+
+    public Tell(List params) {
+        super(params);
+        turtlesIDList = breakIDList(stripBrackets(param1));
+    }
+
+
+    private String stripBrackets(String s) {
+        String newS;
+        newS = s.replaceAll("\\s*\\[\\s*", "");
+        newS = newS.replaceAll("\\s*\\]\\s*", "");
+        return newS;
+    }
+
+    private String[] breakIDList(String s) {
+        return s.split("\\s");
+    }
+
 }
