67d15d4080ce382ab8a76f99d406005de27e9883
Jason Zhou <jz192@duke.edu>
2018-10-28 21:37:38 -0400

    added mult turtles

--- src/Model/src/commandFactory/CommandFactory.java
+++ src/Model/src/commandFactory/CommandFactory.java
@@ -32,46 +32,45 @@ public class CommandFactory implements CommandFactoryInterface {
         System.out.println(params);
 
         SLogoExecutable command = null;
-        if (variableAccessor == null) {
-            System.out.println("sdvvs");
-        }
         CommandTextWrapper commandTextWrapper = variableAccessor.getCommand(cmd);
 
         if (commandTextWrapper != null) {
             command = new GenericCommand(params, commandTextWrapper);
         }
+        else {
+            String commandName = "commands." + cmd;
+            Class<?> commandClass = null;
+            try {
+                commandClass = Class.forName(commandName);
+            }
+            catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
 
-        String commandName = "commands." + cmd;
-        Class<?> commandClass = null;
-        try {
-            commandClass = Class.forName(commandName);
-        }
-        catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            Constructor<?> constructor = commandClass.getConstructors()[0];
+
+            try {
+                command = (SLogoExecutable) constructor.newInstance(params);
+            }
+
+            catch (InstantiationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
 
-        Constructor<?> constructor = commandClass.getConstructors()[0];
-
-        try {
-            command = (SLogoExecutable) constructor.newInstance(params);
-        }
-
-        catch (InstantiationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (InvocationTargetException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
         if (command == null) {
             throw new NullPointerException("Command Object is null");
         }
