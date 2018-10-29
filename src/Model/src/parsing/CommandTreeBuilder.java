0e6f7e138addb7091270ab2371f8024c290e19fb
Jonathan Nakagawa <jyn2@duke.edu>
2018-10-28 20:16:57 -0400

    Added implementation for groups

--- src/Model/src/parsing/CommandTreeBuilder.java
+++ src/Model/src/parsing/CommandTreeBuilder.java
@@ -58,7 +58,13 @@ public class CommandTreeBuilder implements TreeBuilder {
             return result.getRight();
         }
         else if(type.equals(GROUP_START_KEY)){
-
+            String command = commandNode.getData();
+            buildingNode.addChild(new TreeNode(command));
+            buildingNode = buildingNode.getChildren().get(0);
+            commandNode = commandNode.getChild();
+            Pair<Double, ListNode> result = sumGroup(new Double(0), commandNode);
+            buildingNode.addChild(new TreeNode(result.getLeft().toString()));
+            return result.getRight();
         }
         return null;
     }
@@ -80,10 +86,21 @@ public class CommandTreeBuilder implements TreeBuilder {
             System.out.println("Error");
         }
         else if(myContainer.getType(commandNode.getData()).equals(LIST_END_KEY)){
-            return new Pair<String, ListNode>(curList + " " + commandNode.getData(), commandNode.getChild());
+            return new Pair<>(curList + " " + commandNode.getData(), commandNode.getChild());
         }
         return joinList(curList + " " + commandNode.getData(), commandNode.getChild());
     }
+
+    private Pair<Double, ListNode> sumGroup(Double num, ListNode commandNode){
+        if(commandNode == null){
+            System.out.println("Error");
+        }
+        else if(myContainer.getType(commandNode.getData()).equals(GROUP_END_KEY)){
+            return new Pair<>(num, commandNode.getChild());
+        }
+        return sumGroup(Double.parseDouble(commandNode.getData()), commandNode.getChild());
+    }
+
 
     private ListNode buildList(List<String> lst){
         ListNode strt = new ListNode();
